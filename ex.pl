05b5635e508c85c2f872fc421c34afc91d3e25c6
siva prasad <kuppalasivaprasad11@gmail.com>
2017-03-14 11:34:04 +0530

    two files added

---
+++ ex.pl
@@ -0,0 +1,7 @@
+open(my $fh, '>', 'rep.txt');
+$num_words = "eight";
+$num_word = "eight";
+$num_wor = "eight";
+print $fh "There are $num_words $num_word $num_wor words altogether in this sentence.\n";
+
+close $fh;
