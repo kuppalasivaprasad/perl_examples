230fcd6a5483682f428899354ff2845098b1e66d
siva prasad kuppala <kuppalasivaprasad@users.noreply.github.com>
2017-03-14 11:36:23 +0530

    Update ex.pl

--- ex.pl
+++ ex.pl
@@ -2,6 +2,6 @@ open(my $fh, '>', 'rep.txt');
 $num_words = "eight";
 $num_word = "eight";
 $num_wor = "eight";
-print $fh "There are $num_words $num_word $num_wor words altogether in this sentence.\n";
+print $fh "There are $num_words altogether in this sentence \n";
 
 close $fh;
